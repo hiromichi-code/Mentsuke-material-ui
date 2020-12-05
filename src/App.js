6b3ad70f6bbce9554c4938af4f31d445059602f4
Hiro <hiromichi.s.617@gmail.com>
2020-12-05 09:30:41 +0900

    カウンター作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Box from '@material-ui/core/Box';
+import AndroidIcon from '@material-ui/icons/Android';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
+  handlePlus() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  handleMainus() {
+    this.setState({ count: this.state.count - 1 });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Typography component="countNumber">
+          <Box fontSize="h1.fontSize" m={1}>{this.state.count}</Box>
+        </Typography>
+        <div className="btn">
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            startIcon={<AndroidIcon />}
+            onClick={() => {
+              this.handlePlus();
+            }}
+          >
+            プラス
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            size="large"
+            startIcon={<AndroidIcon />}
+            onClick={() => {
+              this.handleMainus();
+            }}
+          >
+            マイナス
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
